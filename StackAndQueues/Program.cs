8cc7b76f8269ebee272efcadb62bec7aae6b27a3
vishal <vishalpalode9899@gmail.com>
2022-02-03 17:27:09 +0530

    Stack operation using linkedlist

--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -8,11 +8,45 @@ namespace StackAndQueues
 {
     internal class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stack And Queues");
+            Console.WriteLine("Welcome to Stack and Queue program using Linked List");
+            CallStackQueue();
             Console.ReadLine();
-
+        }
+        /// <summary>
+        /// Calls the stack queue.
+        /// </summary>
+        public static void CallStackQueue()
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            bool CONTINUE = true;
+            while (CONTINUE)
+            {
+                Console.WriteLine("\nEnter your option!");
+                Console.WriteLine("1=Stack , 2=Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("<---Creating A Stack--->");
+                        linkedListStack.Push(70);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(56);
+                        linkedListStack.Display();
+                        break;
+                    case 2:
+                        CONTINUE = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option!");
+                        break;
+                }
+            }
         }
     }
 }
